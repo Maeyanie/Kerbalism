fee40ca2adaec356109ded0c509cd3fa212e110f
Maeyanie <me@maeyanie.com>
2018-09-17 21:42:59 -0400

    Spelling/Grammar fix: "looses" -> "loses"
    Unless you actually mean to say that reputation is lost when a kerbal unleashes his marbles.

--- src/System/Preferences.cs
+++ src/System/Preferences.cs
@@ -17,7 +17,7 @@ namespace KERBALISM
 		[GameParameters.CustomParameterUI("Highlight Malfunctions", toolTip = "Highlight faild parts in flight")]
 		public bool highlights = true;
 
-		[GameParameters.CustomIntParameterUI("Breakdown Reputation Penalty", minValue = 0, maxValue = 300, toolTip = "Reputation removed when a Kerbal looses his marbles in space")]
+		[GameParameters.CustomIntParameterUI("Breakdown Reputation Penalty", minValue = 0, maxValue = 300, toolTip = "Reputation removed when a Kerbal loses his marbles in space")]
 		public int breakdownPenalty = 10;
 
 		[GameParameters.CustomIntParameterUI("Death Reputation Penalty", minValue = 0, maxValue = 300, toolTip = "Reputation removed when a Kerbal dies")]
